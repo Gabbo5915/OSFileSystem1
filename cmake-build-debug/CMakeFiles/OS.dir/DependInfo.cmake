ef6320c1133c7212bbb70f321130f1de13366ef9
Gabbo5915 <penggeapp2017@gmail.com>
2018-12-01 18:25:56 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OS.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/.idea/open_path.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/.idea/open_path.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/FileSubstr.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/FileSubstr.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/cat.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/cat.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/cd.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/cd.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/close.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/close.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/creat.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/creat.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/doread.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/doread.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/dowrite.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/dowrite.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/exitsys.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/exitsys.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/fileread.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/fileread.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/filewrite.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/filewrite.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/help.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/help.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/main.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/main.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/mkdir.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/mkdir.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/open.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/open.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/open_path.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/open_path.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/rm.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/rm.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/rmdir.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/rmdir.cpp.o"
+  "/Users/gabbo/Documents/GitHub/OSFileSystem/startsys.cpp" "/Users/gabbo/Documents/GitHub/OSFileSystem/cmake-build-debug/CMakeFiles/OS.dir/startsys.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OS.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS.dir/main.cpp.o"
+  "CMakeFiles/OS.dir/help.cpp.o"
+  "CMakeFiles/OS.dir/cd.cpp.o"
+  "CMakeFiles/OS.dir/startsys.cpp.o"
+  "CMakeFiles/OS.dir/mkdir.cpp.o"
+  "CMakeFiles/OS.dir/rmdir.cpp.o"
+  "CMakeFiles/OS.dir/open.cpp.o"
+  "CMakeFiles/OS.dir/close.cpp.o"
+  "CMakeFiles/OS.dir/.idea/open_path.cpp.o"
+  "CMakeFiles/OS.dir/open_path.cpp.o"
+  "CMakeFiles/OS.dir/FileSubstr.cpp.o"
+  "CMakeFiles/OS.dir/creat.cpp.o"
+  "CMakeFiles/OS.dir/rm.cpp.o"
+  "CMakeFiles/OS.dir/filewrite.cpp.o"
+  "CMakeFiles/OS.dir/dowrite.cpp.o"
+  "CMakeFiles/OS.dir/fileread.cpp.o"
+  "CMakeFiles/OS.dir/doread.cpp.o"
+  "CMakeFiles/OS.dir/exitsys.cpp.o"
+  "CMakeFiles/OS.dir/cat.cpp.o"
+  "OS.pdb"
+  "OS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
